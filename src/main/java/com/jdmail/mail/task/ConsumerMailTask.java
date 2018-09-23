8904807c1363c144d2dc5c4ab156140638db6091
a4goku <wangyi_062500@hotmail.com>
2018-09-23 13:39:29 +0800

    测试邮箱，成功

--- src/main/java/com/jdmail/mail/task/ConsumerMailTask.java
+++ src/main/java/com/jdmail/mail/task/ConsumerMailTask.java
@@ -7,6 +7,16 @@ import org.springframework.stereotype.Component;
 public class ConsumerMailTask {
     @Scheduled(initialDelay = 5000, fixedDelay = 2000)
     public void intervalFast(){
-        System.out.println("开始执行。。。。。。");
+        System.out.println("fast开始执行。。。。。。");
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void intervalNormal(){
+        System.out.println("normal开始执行。。。。。。");
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 60000)
+    public void intervalDefer(){
+        System.out.println("defer开始执行。。。。。。");
     }
 }
--- src/test/java/com/jdmail/mail/MailConsumerApplicationTests.java
+++ src/test/java/com/jdmail/mail/MailConsumerApplicationTests.java
@@ -2,8 +2,13 @@ package com.jdmail.mail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +17,21 @@ public class MailConsumerApplicationTests {
     @Test
     public void contextLoads() {
     }
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendSimpleMail() throws Exception{
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("wangyi_062500@163.com");
+        message.setTo("wangyi_062500@hotmail.com");
+        message.setSubject("主题：这是一封用代码写的测试邮件"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        message.setText("Test content");
+
+        mailSender.send(message);
+    }
+
+
 
 }
